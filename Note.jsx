df940c7a30edc16ad7ff60ba591b5cc36414486d
Lima Ghosh <84917028+LimaGhosh@users.noreply.github.com>
2021-06-28 10:50:17 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js </h1>
+      <p>
+        {" "}
+        A basic web dev React Js Bootcamp
+      </p>
+    </div>
+  );
+}
+
+export default Note;
